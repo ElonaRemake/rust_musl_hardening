ae1441af3328cdb7a8a4567f72b0daf5e5151d0d
Alissa Rao <lymia@lymiahugs.com>
2022-10-21 11:11:11 -0700

    Final refinements to get this working well.

--- common.inc.sh
+++ common.inc.sh
@@ -2,6 +2,15 @@ SUBMODULE_SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/nul
 CONTRIB_ROOT_PATH="$(realpath "$SUBMODULE_SCRIPT_DIR/..")"
 cd "$CONTRIB_ROOT_PATH" || exit 1
 
+# Configuration
+ALPINE_MIRROR="https://dl-cdn.alpinelinux.org/alpine"
+
+APK_TOOLS_FILENAME="apk-tools-static-2.12.9-r3.apk"
+APK_TOOLS_URL="$ALPINE_MIRROR/v3.16/main/x86_64/$APK_TOOLS_FILENAME"
+
+ALPINE_KEYS_FILENAME="alpine-keys-2.4-r1.apk"
+ALPINE_KEYS_URL="$ALPINE_MIRROR/v3.16/main/x86_64/$ALPINE_KEYS_FILENAME"
+
 # Common functions
 root_path() {
   # Absolute path so we don't end up using the Alpine Linux realpath
@@ -33,18 +42,27 @@ use_musl_root() {
   export CC="$(root_path musl_root/usr/bin/clang)"
   export CXX="$(root_path musl_root/usr/bin/clang++)"
 
-  export LD_LIBRARY_PATH="$(root_path musl_root/lib):$(root_path musl_root/usr/lib)"
+  export LD_LIBRARY_PATH="$(root_path musl_usr/lib):$(root_path musl_root/lib):$(root_path musl_root/usr/lib)"
   export LIBRARY_PATH="$(root_path musl_usr/lib):$(root_path musl_root/lib):$(root_path musl_root/usr/lib)"
-  export PATH="$(root_path musl_root/bin):$(root_path musl_root/sbin):$(root_path musl_root/usr/bin):$(root_path musl_root/usr/sbin)"
-  export C_INCLUDE_PATH="$(root_path musl_root/usr/include)"
-  export CPLUS_INCLUDE_PATH="$(root_path musl_root/usr/include)"
+  export PATH="$(root_path musl_usr/bin):$(root_path musl_root/bin):$(root_path musl_root/sbin):$(root_path musl_root/usr/bin):$(root_path musl_root/usr/sbin)"
+  export C_INCLUDE_PATH="$(root_path musl_usr/include):$(root_path musl_root/usr/include)"
+  export CPLUS_INCLUDE_PATH="$(root_path musl_usr/include):$(root_path musl_root/usr/include)"
 
-  export CFLAGS="$CFLAGS_COMMON_SIZE"
-  export LDFLAGS="$CFLAGS_COMMON_SIZE"
+  export CFLAGS="$CFLAGS_COMMON"
+  export LDFLAGS="$LDFLAGS_COMMON"
 
   export APK="apk -p $(root_path musl_root)"
   export ABUILD_SHAREDIR="$(root_path musl_root/usr/share/abuild)"
   export FAKEROOT="fakeroot -l $(root_path musl_root/usr/lib/libfakeroot.so)"
+}
+
+use_hardened_flags() {
+  export CFLAGS="$(strip_flags "$CFLAGS_COMMON $1")"
+  export LDFLAGS="$(strip_flags "$CFLAGS_COMMON $1 $2")"
+}
+use_unhardened_flags() {
+  export CFLAGS="$(strip_flags "$CFLAGS_COMMON_UNHARDENED $1")"
+  export LDFLAGS="$(strip_flags "$CFLAGS_COMMON_UNHARDENED $1 $2")"
 }
 
 strip_flags() {
@@ -52,30 +70,36 @@ strip_flags() {
   echo -n " $1 " | tr -d "\n" | sed -e "s/\s\+/ /g"
 }
 
+apk_add_package() {
+  apk \
+    -X "$ALPINE_MIRROR/edge/main" -U -p musl_root --initdb --cache-dir "$(root_path packages/apk_cache)" add \
+    "$@"
+}
+
 # Common C Flags
-BUILD_TARGET="x86_64-unknown-linux-gnu"
+BUILD_TARGET="x86_64-unknown-linux-musl"
+if [ "$OPT_LEVEL" = "" ]; then
+  OPT_LEVEL="-Os"
+fi
 
-CFLAGS_BASIC="
-  -target $BUILD_TARGET -rtlib=compiler-rt -L $(root_path musl_root/lib) -L $(root_path musl_root/usr/lib)
-"
-CFLAGS_LTO="
+CFLAGS_BASIC="$(strip_flags "
+  $OPT_LEVEL -target $BUILD_TARGET
+  -rtlib=compiler-rt -L$(root_path musl_root/usr/lib) -L$(root_path musl_root/lib)
+  -static -Wl,--static
+")"
+CFLAGS_LTO="$(strip_flags "
   -flto=full -fuse-ld=lld
-"
-CFLAGS_HARDENING_BASIC="
-  -D_FORTIFY_SOURCE=2 -fPIE
-  -fstack-protector-strong -fsanitize=safe-stack -fPIE -fstack-clash-protection
-"
-CFLAGS_HARDENING="
+")"
+CFLAGS_HARDENING_BASIC="$(strip_flags "
+  -D_FORTIFY_SOURCE=2 -fPIE -fstack-protector-strong -fstack-clash-protection
+")"
+CFLAGS_HARDENING="$(strip_flags "
   $CFLAGS_LTO $CFLAGS_HARDENING_BASIC
-  -fsanitize=cfi -fvisibility=hidden -fvirtual-function-elimination
-"
+  -fsanitize=cfi -fvisibility=hidden -fvirtual-function-elimination -fsanitize=safe-stack
+")"
 
-CFLAGS_COMMON_SIZE="-Os -static $(strip_flags "$CFLAGS_BASIC") $(strip_flags "$CFLAGS_HARDENING")"
-CFLAGS_COMMON_OPT="-O2 -static $(strip_flags "$CFLAGS_BASIC") $(strip_flags "$CFLAGS_HARDENING")"
-
-CFLAGS_COMMON_UNHARDENED_SIZE="-Os -static $(strip_flags "$CFLAGS_BASIC") $(strip_flags "$CFLAGS_LTO")"
-CFLAGS_COMMON_UNHARDENED_OPT="-O2 -static $(strip_flags "$CFLAGS_BASIC") $(strip_flags "$CFLAGS_LTO")"
-
-CFLAGS_ABULID="-O2 $(strip_flags "$CFLAGS_BASIC") $(strip_flags "$CFLAGS_HARDENING_BASIC")"
+CFLAGS_COMMON="$CFLAGS_BASIC $CFLAGS_HARDENING"
+CFLAGS_COMMON_UNHARDENED="$CFLAGS_BASIC $CFLAGS_LTO"
+CFLAGS_ABULID_MUSL="$CFLAGS_BASIC $CFLAGS_HARDENING_BASIC"
 
 MAKE_CT="-j$(nproc --all)" # Flag for number of processors for `make`
--- setup_musl_env.sh
+++ setup_musl_env.sh
@@ -3,71 +3,66 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . $SCRIPT_DIR/common.inc.sh
 
-# Configuration
-MIRROR="https://dl-cdn.alpinelinux.org/alpine"
-
-APK_TOOLS_FILENAME="apk-tools-static-2.12.9-r3.apk"
-APK_TOOLS_URL="$MIRROR/v3.16/main/x86_64/$APK_TOOLS_FILENAME"
-
-ALPINE_KEYS_FILENAME="alpine-keys-2.4-r1.apk"
-ALPINE_KEYS_URL="$MIRROR/v3.16/main/x86_64/$ALPINE_KEYS_FILENAME"
-
 # Recreate the relevant directories
 rm -rf build
-mkdir build || exit
+mkdir build || exit 1
 
 rm -rf musl_root
-mkdir musl_root || exit
+mkdir musl_root || exit 1
+
+if [ ! -d packages ]; then
+  mkdir packages || exit 1
+fi
+if [ ! -d packages/apk_cache ]; then
+  mkdir packages/apk_cache || exit 1
+fi
 
 # Download and extract the bootstrap
-mkdir build/apk || exit
-cd build/apk || exit
-  wget $APK_TOOLS_URL $ALPINE_KEYS_URL || exit
-  sha256sum -c ../../src/alpine_sha256sum || exit
+mkdir build/apk || exit 1
+cd build/apk || exit 1
+  wget $APK_TOOLS_URL $ALPINE_KEYS_URL || exit 1
+  sha256sum -c ../../src/alpine_sha256sum || exit 1
 
-  tar -x -f $APK_TOOLS_FILENAME || exit
-  tar -x -f $ALPINE_KEYS_FILENAME || exit
+  tar -x -f $APK_TOOLS_FILENAME || exit 1
+  tar -x -f $ALPINE_KEYS_FILENAME || exit 1
 
   export PATH="$(realpath sbin):$PATH"
   export APK_KEYS_PATH="$(realpath usr/share/apk/keys)"
-cd ../.. || exit
+cd ../.. || exit 1
 
 # Bootstrap Alpine Linux
 apk.static \
-  -X "$MIRROR/edge/main" -U -p musl_root --initdb \
+  -X "$ALPINE_MIRROR/edge/main" -U -p musl_root --initdb \
   --keys-dir "$APK_KEYS_PATH" --cache-dir "$(root_path packages/apk_cache)" add \
-  alpine-base alpine-sdk libc6-compat llvm15 clang15 gcc lld libstdc++-dev compiler-rt
+  alpine-base alpine-sdk musl musl-dev llvm15 clang15 gcc lld libstdc++-dev compiler-rt
 
 # Activate the musl root since we're about to do some custom builds
 use_musl_root
-"$(/usr/bin/which busybox)" --install musl_root/bin || exit
-/usr/bin/sed --in-place=.bak "s_/bin/ash -e_/usr/bin/env ash_" musl_root/usr/bin/abuild || exit
+"$(/usr/bin/which busybox)" --install musl_root/bin || exit 1
+/usr/bin/sed --in-place=.bak "s_/bin/ash -e_/usr/bin/env ash_" musl_root/usr/bin/abuild || exit 1
 
 # Setup abuild and our new repository
 export REPODEST="$(root_path build/packages)"
-mkdir "$REPODEST" || exit
+mkdir "$REPODEST" || exit 1
 if [ ! -d ~/.abuild ]; then
-  abuild-keygen -q -n -a || exit
+  abuild-keygen -q -n -a || exit 1
 fi
 
 # Do the custom musl build
-cd build || exit
-  cp -r "$SCRIPT_DIR/musl" musl || exit
-  cd musl || exit
-    export CFLAGS="$CFLAGS_ABULID"
+cd build || exit 1
+  cp -r "$SCRIPT_DIR/musl" musl || exit 1
+  cd musl || exit 1
     SAFESTACK_O="$(root_path musl_root/usr/lib/clang)"
     SAFESTACK_O="$(echo "$SAFESTACK_O"/*/lib/linux/libclang_rt.safestack-x86_64.a)"
-    export LDFLAGS="$LDFLAGS -Wl,$SAFESTACK_O"
-    abuild || exit
-  cd .. || exit
-cd .. || exit
+    export CFLAGS="$CFLAGS_ABULID_MUSL"
+    export LDFLAGS="$LDFLAGS_ABULID_MUSL -Wl,$SAFESTACK_O"
+    abuild || exit 1
+  cd .. || exit 1
+cd .. || exit 1
 
 # Install our custom packages
 apk \
-  -X "$REPODEST/build" -U -p musl_root --keys-dir ~/.abuild --cache-dir "$(root_path packages/apk_cache)" \
-  add libc6-compat musl musl-dbg musl-dev musl-libintl musl-utils
-apk \
-  -X "$REPODEST/build" -U -p musl_root --keys-dir ~/.abuild --cache-dir "$(root_path packages/apk_cache)" \
+  -X "$REPODEST/build" -U -p musl_root --keys-dir ~/.abuild --no-cache \
   upgrade
 
 # Delete the build directory
